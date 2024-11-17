8deaa89b35cef54cde88b5de56e090e6aea77653
FSO_01_하태웅_ <hamasu271@gmail.com>
2024-11-18 00:02:11 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addToCart } from '../redux/redux';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+function Menu() {
+  const menu = useSelector((state) => state.menu.menu);
+  const dispatch = useDispatch();
+
+  const handleAddToCart = (item) => {
+    dispatch(addToCart(item));
+  };
+
+  return (
+    <div>
+      <h1>Menu</h1>
+      <ul>
+        {menu.map((item) => (
+          <li key={item.id}>
+            {item.name} - ${item.price}
+            <button onClick={() => handleAddToCart(item)}>Add to Cart</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Menu;
